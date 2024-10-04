9f60744f535d511c54e06812d028dc0e54ac7529
максим бобков <makwest5008@gmail.com>
2024-10-04 19:11:30 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Authorization from "./components/authorization/registration";
+
+export default function App() {
+  return (
+    <div>
+        <Header />
+        <Authorization />
+        {/*<Footer />*/}
+    </div>
+  );
+}
