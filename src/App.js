d33f1995569b1106d78007e5132ba84837b5b23a
максим бобков <makwest5008@gmail.com>
2024-10-05 22:57:33 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Footer from "./components/footer/footer";
-import Header from "./components/header/header";
 import Authorization from "./components/authorization/authorization";
 import Registration from "./components/registration/Registration";
 
